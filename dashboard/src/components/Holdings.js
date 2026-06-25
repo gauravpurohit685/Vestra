0e4e3be636a6326decaf30d1d76c92cd05619583
Gaurav Purohit <gauravpurohit685@gmail.com>
2026-06-25 23:50:41 +0530

    feat: holdings page creatrd successfully

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import { holdings } from "../data/data";
+
 const Holdings = () => {
   return (
     <>
-      <h3 className="title">Holdings (13)</h3>
+      <h3 className="title">Holdings ({holdings.length})</h3>
 
       <div className="order-table">
         <table>
@@ -17,26 +19,39 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
+
+          {
+            holdings.map((stocks, index) => {
+              const currPrice = stocks.price - stocks.qty;
+              const isProfit = currPrice - stocks.avg - stocks.qty >= 0.0;
+              const profClass = isProfit? "profit" : "loss";
+              const dayClass = stocks.isLoss ? "loss": "profit"
+
+              return (
+                <tr key={index}>
+                  <td>{stocks.name}</td>
+                  <td>{stocks.name}</td>
+                  <td>{stocks.avg.toFixed(2)}</td>
+                  <td>{stocks.price.toFixed(2)}</td>
+                  <td>{currPrice.toFixed(2)}</td>
+                  <td className={profClass}>
+                      {(currPrice - stocks.avg * stocks.qty).toFixed(2)}
+                  </td>
+                  <td className={profClass}>
+                    {stocks.net}
+                  </td>
+                  <td className={dayClass}>
+                    {stocks.day}
+                  </td>
+                </tr>
+              )
+            })
+          }
         </table>
       </div>
 
       <div className="row">
-        <div className="col">
-          <h5>
-            29,875.<span>55</span>{" "}
-          </h5>
-          <p>Total investment</p>
-        </div>
-        <div className="col">
-          <h5>
-            31,428.<span>95</span>{" "}
-          </h5>
-          <p>Current value</p>
-        </div>
-        <div className="col">
-          <h5>1,553.40 (+5.20%)</h5>
-          <p>P&L</p>
-        </div>
+        
       </div>
     </>
   );
