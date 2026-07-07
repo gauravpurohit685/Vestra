8b46100d4b75c7f9053bd9b65cadcbdde56c1b05
Gaurav Purohit <gauravpurohit685@gmail.com>
2026-07-07 17:33:08 +0530

    feat: backend websocket configured successfully

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const http = require("http");
+const {Server} = require("socket.io");
+const WebSocket = require("ws");
+
 const connectDB = require("./src/config/database");
 const authRouter = require("./src/routes/authRouter");
 const positionRouter = require("./src/routes/positionRouter");
@@ -9,11 +13,27 @@ const holdingRouter = require("./src/routes/holdingRouter");
 const bankRouter = require("./src/routes/bankRouter");
 const orderRouter = require("./src/routes/orderRouter");
 
+require("dotenv").config();
+
+const allowedOrigins = [
+    "http://localhost:1234", // Landing
+    "http://localhost:3000"  // Dashboard
+];
+
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: allowedOrigins,
+        credentials: true,
+        methods: ["GET", "POST", "PATCH", "DELETE"]
+    }
+});
 
 app.use(
     cors({
-        origin: "http://localhost:1234",
+        origin: allowedOrigins,
         credentials: true,
         methods: ["GET", "POST", "PATCH", "DELETE"]
     })
@@ -31,10 +51,78 @@ app.use("/", orderRouter);
 app.use("/", positionRouter);
 app.use("/", profileRouter);
 
+io.on("connection", (socket) => {
+    console.log("Connected: ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected: ", socket.id);
+    })
+});
+
+const finnhubSocket = new WebSocket(
+    `wss://ws.finnhub.io?token=${process.env.FINNHUB_API_KEY}`
+);
+
+const companies = [
+    "AAPL",
+    "MSFT",
+    "KO",
+    "PEP",
+    "WMT",
+    "MCD",
+    "JNJ",
+    "PG",
+    "V",
+    "MA",
+    "IBM",
+    "ORCL",
+    "CSCO",
+    "DIS",
+    "NKE"
+];
+
+finnhubSocket.on("open", () => {
+
+    console.log("Connected to Finnhub");
+
+    companies.forEach(symbol => {
+
+        finnhubSocket.send(
+            JSON.stringify({
+                type: "subscribe",
+                symbol
+            })
+        );
+
+    });
+
+});
+
+finnhubSocket.on("message", (message) => {
+
+    const parsedData = JSON.parse(message);
+
+    if (parsedData.type !== "trade")
+        return;
+
+    io.emit("market-update", parsedData.data);
+
+});
+
+finnhubSocket.on("error", (err) => {
+
+    console.log(err);
+
+});
+
+finnhubSocket.on("close", () => {
+    console.log("Finnhub Websocket disconneted successfully!");
+});
+
 
 connectDB().then(() => {
     console.log("Database connection established successfully!");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
         console.log("The server is listening successfully!");
     })
 })
