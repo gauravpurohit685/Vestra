8fbe62e8547cd989c05814800f1fbefc5962ed6a
Gaurav Purohit <gauravpurohit685@gmail.com>
2026-07-17 18:25:38 +0530

    feat: UI changes made in dashboard

--- dashboard/src/components/TopBar.js
+++ dashboard/src/components/TopBar.js
@@ -7,13 +7,8 @@ const TopBar = () => {
     <div className="topbar-container">
       <div className="indices-container">
         <div className="nifty">
-          <p className="index">NIFTY 50</p>
-          <p className="index-points">{100.2} </p>
-          <p className="percent"> </p>
-        </div>
-        <div className="sensex">
-          <p className="index">SENSEX</p>
-          <p className="index-points">{100.2}</p>
+          <p className="index">Currency</p>
+          <p className="index-points">{"USD($)"} </p>
           <p className="percent"></p>
         </div>
       </div>
